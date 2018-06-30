1faf9de0e71d9b107d96afb871ae5e951ab948aa
Erera <erera@bk.ru>
2018-06-30 14:33:39 +0300

    line wrapping

--- src/ResultProcessing.java
+++ src/ResultProcessing.java
@@ -42,24 +42,30 @@ public class ResultProcessing extends JFrame {
             textArea.append("Your cat's weight is absolutely normal. \n " +
                     "Feed your cat as usually, diet is unnecessary.\n" +
                     "\n Remember that your cat should eat at least twice a day;\n" +
-                    "\n if you feed your cat with natural food, it's recommended to give your cat " + String.format("%.2f", weight * 50 / 2) +
-                    " grams of meat per day and " + String.format("%.2f", weight * 50 / 2) + " grams of fermented milk products. Also add 10-15 grams of vegetables per day;");
+                    "\n if you feed your cat with natural food, it's recommended to give your cat "
+                    + String.format("%.2f", weight * 50 / 2) +
+                    " grams of meat per day and " + String.format("%.2f", weight * 50 / 2) +
+                    " grams of fermented milk products. Also add 10-15 grams of vegetables per day;");
         }
         if (result == 1) {
             textArea.append("Your cat is overweight. \n " +
                     "You should give more attention to the diet of your cat. Here are some recommendations:\n" +
                     "\n- buy diet cat food and feed your cat according to the instructions written on the package;\n" +
-                    "\n- if you feed your cat with natural food, it's recommended to give your cat " + String.format("%.2f",weight*45/2)+
-                    " grams of meat per day and " + String.format("%.2f",weight*45/2) + " grams of fermented milk products. Also add 10-15 grams of vegetables per day;\n" +
+                    "\n- if you feed your cat with natural food, it's recommended to give your cat "
+                    + String.format("%.2f",weight*45/2)+
+                    " grams of meat per day and " + String.format("%.2f",weight*45/2) +
+                    " grams of fermented milk products. Also add 10-15 grams of vegetables per day;\n" +
                     "\n- don't overfeed the cat. Cats ask for food even if they are not hungry;\n" +
                     "\n- your cat should eat at least twice a day;");
         }
         if (result == 2) {
             textArea.append("Your cat is underweight. \n " +
                     "You should give more attention to the diet of your cat. Here are some recommendations:\n" +
-                    "\n- buy cat food for cats with a lack of weight and feed your cat according to the instructions written on the package;\n" +
+                    "\n- buy cat food for cats with a lack of weight and feed your cat according " +
+                    "to the instructions written on the package;\n" +
                     "\n- if you feed your cat with natural food, it's recommended to give your cat " + String.format("%.2f",weight*60/2)+
-                    " grams of meat per day and " + String.format("%.2f",weight*60/2) + " grams of fermented milk products. Also add 10-15 grams of vegetables per day;\n" +
+                    " grams of meat per day and " + String.format("%.2f",weight*60/2) +
+                    " grams of fermented milk products. Also add 10-15 grams of vegetables per day;\n" +
                     "\n- your cat should eat at least three times a day;");
         }
 
